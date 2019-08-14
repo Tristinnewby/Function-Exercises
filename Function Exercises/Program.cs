484514357b18f159cb403fe08e68665cad01b48b
Tristinnewby <tristinnewby@outlook.com>
2019-08-14 10:57:41 -0700

    Added VS Program

---
+++ Function Exercises/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function_Exercises
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
