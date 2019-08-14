357eee070c383923ed8800c4100e917cd2621fc7
Tristinnewby <tristinnewby@outlook.com>
2019-08-14 11:54:10 -0700

    Finished Exercise

--- Function Exercises/Program.cs
+++ Function Exercises/Program.cs
@@ -10,7 +10,38 @@ namespace Function_Exercises
     {
         static void Main()
         {
+            Game game = new Game();
+            game.score = 100;
+            game.Start();
+            Console.Read();
+        }
+    }
 
+    class Game
+    {
+        public int score = 0;
+
+        public void Start()
+        {
+            ChangeScore(5);
+            PrintScore();
+            ChangeScore(2);
+            PrintScore();
+            ChangeScore(12);
+            PrintScore();
+            ChangeScore(8);
+            PrintScore();
+        }
+
+        public int ChangeScore(int val)
+        {
+            score += val;
+            return score;
+        }
+
+        public void PrintScore()
+        {
+            Console.WriteLine("Score: " + score);
         }
     }
 }
